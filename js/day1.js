4a7065c94b41b279f794fcd23d4626255f22507c
RajeshPandey10 <imrajesh2005@gmail.com>
2025-05-22 22:32:16 +0545

    js started

---
+++ js/day1.js
@@ -0,0 +1,95 @@
+// var a =20;
+// var a =10;
+// var c = '10';
+// console.log(a)
+// var a=null;
+// console.log(typeof(a));
+
+
+// let name1 ="shyam"
+// let name2 ="ram"
+// let name3 ="hari"
+// console.log(name1,name2,name3)
+//  let names = [1,2,3,4,5,6]
+//  [1,2,3,4,5].splice
+//  names.push("ravi");
+// console.log(names.slice(0,2,1))
+// let day = ["sunday",'monday',"tuesday",'wednesday','thu','fri','sat']
+// let sliced = day.splice(day.indexOf("monday"),day.indexOf("thu"))
+// console.log(sliced)
+// console.log(day)
+// console.log(newname)
+// console.log(names)
+// names[4]="haribahadur"
+// console.log(names)
+//  names.pop()
+// names.shift()
+// names.unshift("digital gurukul")
+// console.log(names)
+//  let doesExist = names.includes()
+//  console.log(doesExist)
+//  console.log(typeof(names[1]))
+//  console.log(names.length)
+
+//object
+// let person = {
+// name:"Rajesh pandey",
+// age:22,
+// address:"dhran"
+// }
+// Object.freeze(person);
+
+//k object vutra array ra arrray vitra object possible xa?
+//  person.address="ktm"
+// console.log(person.address ,person.age)
+
+// let response = {
+//     "success": true,
+ //    
+//res=https://digital-gurukul-server.vercel.app/api/courses;
+// (res.status ==200)
+
+
+//     "count": 2,
+//     "courses": [
+//         {
+//             id: "6814eef9dbe97a83db354425",
+//             title: "World Domination Planning",
+//             description: "This course will teach you how to plan strategies for world domination with practical example",
+//             thumbnail: "https://res.cloudinary.com/djmsdn5yo/image/upload/v1746645594/digital-gurukul/drh1hc5kgygia5ahayhb.jpg",
+//             instructor: "Unknown",
+//             price: 3000,
+//             level: "Beginner",
+//             duration: "52 weeks",
+//         },
+        
+//         {
+//             id: "6814eef9dbe97a83db354425",
+//             title: "World Domination Planning",
+//             description: "This course will teach you how to plan strategies for world domination with practical example",
+//             thumbnail: "https://res.cloudinary.com/djmsdn5yo/image/upload/v1746645594/digital-gurukul/drh1hc5kgygia5ahayhb.jpg",
+//             instructor: "Unknown",
+//             price: 4000,
+//             level: "Beginner",
+//             duration: "52 weeks",
+//         }
+//     ]
+// }
+// console.log(response.courses[1].price)
+// int arr = [10,20,30]
+// arr[2]
+
+// map((value,index)=>{
+// console.log(i)
+// })
+
+var isFounde =true;
+var a='20'
+
+if(isFounde && a==20){
+    console.log("lets end the class")
+    return
+}
+
+
+console.log("aja ititshree garaum")
